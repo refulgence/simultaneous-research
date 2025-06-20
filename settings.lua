09714a773415ea44170eb16f714b1af25c2c4466
refulgence <oscuritas@yandex.ru>
2025-06-20 23:45:26 +0300

    other: change the default value of labs processed per second setting and it description

--- settings.lua
+++ settings.lua
@@ -13,7 +13,7 @@ data:extend(
         order = "aa",
         name = "sr-labs-per-second-processed", 
         setting_type = "startup",
-        default_value = 600,
+        default_value = 12,
         allowed_values = {1,2,3,4,5,6,10,12,15,20,30,60,120,180,240,300,360,600,1200,1800,2400}, --Should be a clean divisor or multiple of 60 or it would be less accurate
     },
     -- {
